8591a906e84f5268e12245c024ea49c1f181a6fd
vishal2152 <2010030030@klh.edi.in>
2022-01-20 11:27:19 +0530

    hello

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+def myFunction(say): #you can add variables to the function
+  print(say)
+
+myFunction("Hello")
+
+age = input("How old are you?")
+
+myFunction("You are {} years old!".format(age))
+
+#this is what you get:
+
+Hello
+How old are you?
+>>11 #lol my real age actually
+You are 11 years old!
